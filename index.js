75b5038a20afadc342370e5fdbdf72d6196c41e1
JanSzewczyk <jan.szewczyk1997@gmail.com>
2022-04-11 22:34:54 +0200

    refactor(index.js): remove console log

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-console.log(
-    [
-        'Hi user :)',
-        'I am eslint-config-szum-tech library',
-        'I am the best eslint configuration in the world!!',
-        'But I am still in progress.....',
-        ':(('
-    ].join('\n')
-)
-
-
 module.exports = {
         rules: {
                 // The rules below are listed in the order they appear on the eslint
