5c382119fa6d42bba660ddf10d2bf7507f697eb3
JanSzewczyk <jan.szewczyk1997@gmail.com>
2022-04-13 23:38:27 +0200

    feat(release): create new release config file

---
+++ release.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "docs/CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["docs/CHANGELOG.md"],
+      },
+    ],
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
+};
